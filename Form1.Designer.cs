5892dc865bc6b22628c23ffb38f8f24da9dd5489
sadeval <162736837+sadeval@users.noreply.github.com>
2024-09-28 12:33:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace DancingProgressBars
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+
+        private void InitializeComponent()
+        {
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+
+            // numericUpDown
+            this.numericUpDown.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown.Minimum = 1;
+            this.numericUpDown.Maximum = 20; // Максимальное количество прогресс-баров
+            this.numericUpDown.Value = 5;
+
+            // btnStart
+            this.btnStart.Location = new System.Drawing.Point(12, 50);
+            this.btnStart.Text = "Начать";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            // flowLayoutPanel
+            this.flowLayoutPanel.Location = new System.Drawing.Point(12, 90);
+            this.flowLayoutPanel.Size = new System.Drawing.Size(300, 200);
+            this.flowLayoutPanel.AutoScroll = true;
+
+            // Form1
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DancingProgressBars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int count = (int)numericUpDown.Value;
+            flowLayoutPanel.Controls.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                ProgressBar progressBar = new ProgressBar();
+                progressBar.Width = 200;
+                progressBar.Height = 20;
+                flowLayoutPanel.Controls.Add(progressBar);
+
+                StartProgressBar(progressBar);
+            }
+        }
+
+        private async void StartProgressBar(ProgressBar progressBar)
+        {
+            progressBar.Value = 0;
+            Random random = new Random();
+            int duration = random.Next(5, 11);
+
+            for (int i = 0; i <= 100; i++)
+            {
+                await Task.Delay(random.Next(50, 201));
+
+                if (i % 10 == 0) 
+                {
+                    progressBar.Invoke((MethodInvoker)(() =>
+                    {
+                        progressBar.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                    }));
+                }
+
+                progressBar.Invoke((MethodInvoker)(() => progressBar.Value = i));
+            }
+        }
+
+    }
+}
